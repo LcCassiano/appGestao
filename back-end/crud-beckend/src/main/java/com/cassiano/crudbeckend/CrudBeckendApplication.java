9a5d5751dadc629c3ac8c7c979d752084146b0f0
LcCassiano <lucascassianomm@gmail.com>
2024-01-29 12:51:59 -0300

    beckend created

---
+++ back-end/crud-beckend/src/main/java/com/cassiano/crudbeckend/CrudBeckendApplication.java
@@ -0,0 +1,13 @@
+package com.cassiano.crudbeckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBeckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBeckendApplication.class, args);
+	}
+
+}
---
+++ back-end/crud-beckend/src/test/java/com/cassiano/crudbeckend/CrudBeckendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassiano.crudbeckend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBeckendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
